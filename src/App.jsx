b33f5548283edb4fc6f6d4690061ddf87587a4e9
Doug Hirth <dmh838@hotmail.com>
2025-09-09 12:22:43 -0700

    Shortened ID for Host

--- src/App.jsx
+++ src/App.jsx
@@ -9,109 +9,103 @@ function App() {
   const [conn, setConn] = useState([]);               //This is the connection to another peer.
   const [isHost, setIsHost] = useState(false);          //This is set to true if the browser is the host.
   const [connectionID, setConnectionID] = useState(""); //This is taken from the input field.
-  console.log("------------------");
+
+  const hostPeerIDPrefix = "elduganocatangame-";
 
   useEffect(() => {
-    console.log("Startin useEffect");
     if(newestConn != null){
-      console.log("We had a newestConn.");
+      const playerNumber = conn.length; //This would want to be more sophisticated, incase a player drops out or something.
+            //We might not even really need this, and could just pass in newestConn.peer,
+            //Then hold peer names in an array.
+      newestConn.on('open', function() {
+        // Receive messages
+        newestConn.on('data', function(data,) {
+          //console.log('Received',newestConn.peer, data);
+          console.log('Received from:',playerNumber,'. Message:', data);
+        });
+
+        // Send messages
+        if (isHost)
+          newestConn.send("You listen here, I am the host, so I am the boss. You put in my ID, so I am incharge here!");
+        else
+          newestConn.send("Hey man, I don't want any trouble, I put your ID into my browser, so I'm listening to you!");
+      });
       let newConn = [...conn];
       newConn.push(newestConn);
       setConn(newConn);
       setNewestConn(null);
     }
-    let myPromise;
-    if(myPeerID == null) {
-      var newPeer = new Peer();           //Leave this blank to generate a unique random ID.
-          //We can pass a string like elduganocatangame-[random string], then only show the random string to enter.
-      myPromise = new Promise((resolve, reject) => {
-        newPeer.on('open', function(id) {
-          console.log('My peer ID is: ' + id);
-          setMyPeerID(id);
-          setPeer(newPeer);
-          resolve(newPeer);
-        });  
-      })
-      //newPeer.on('open', function(id) {
-      //  console.log('My peer ID is: ' + id);
-      //  setMyPeerID(id);
-      //  setPeer(newPeer);
-      //});
-      console.log("NEXT!");
-      myPromise.then(result =>{
-        console.log("We did the prmoise thing. here is our peer");
-        console.log(result);
-      });
-
-    }
-  }, [myPeerID, conn, newestConn]);
-
-  console.log("||||| THIS IS MY PEER |||||");
-  console.log(peer);
-  console.log("||||| This is my conn |||||");
-  console.log(conn[0]);
-
-  if (myPeerID != null) {
-    console.log("myPeerID is set, looking at the peer.on");
-    peer.on('connection', function(newConn) {
-      console.log("We just connected!");
-      console.log(newConn);
-      setNewestConn(newConn);
-      setIsHost(true);
-    });
-  }
-
-
-  if (conn.length != 0) {
-    conn[0].on('open', function() {
-      // Receive messages
-      conn[0].on('data', function(data) {
-        console.log('Received', data);
-      });
-
-      // Send messages
-      if (isHost)
-        conn[0].send("You listen here, I am the host, so I am the boss. You put in my ID, so I am incharge here!");
-      else
-        conn[0].send("Hey man, I don't want any trouble, I put your ID into my browser, so I'm listening to you!");
-    });
-    if(conn.length == 2){
-      console.log("conn length is 2.")  //We probably only want this to occure once, really.
-      conn[1].on('open', function() {
-        // Receive messages
-        conn[1].on('data', function(data) {
-          console.log('Received', data);
-        });
-
-        // Send messages
-        if (isHost)
-          conn[1].send("You listen here, I am the host, so I am the boss. You put in my ID, so I am incharge here!");
-        else
-          conn[1].send("This is a message that should never be sent, because only the host has more than 1 connection.");
-      });
-    }
-  }
+  }, [myPeerID, conn, newestConn, isHost]);
 
   const connectionButton= () => {
     //This is the client side.
     console.log("Clicked connection button.");
-    console.log(conn);
-    console.log(connectionID);
-    let newConn = peer.connect(connectionID);
-    console.log(newConn);
-    setConn([newConn]);
+    //let connectionID = hostPeerIDPrefix+connectionID;
+    console.log(hostPeerIDPrefix+connectionID);
+    let newConn = peer.connect(hostPeerIDPrefix+connectionID);
+    setNewestConn(newConn);
   }
   const sendAnotherMessageButton = () => {
     conn[0].send("Do you read me? You were the first connector.");
-    conn[1].send("Do you read me? You were the second connector.");
+    if (conn.length==2)
+      conn[1].send("Do you read me? You were the second connector.");
   }
+
+  const setHost = () => {
+    let newPeerPromise;
+    if(myPeerID == null) {
+      var shortID=makeid(6)
+      var newPeer = new Peer(hostPeerIDPrefix+shortID);
+      newPeerPromise = new Promise((resolve, reject) => {
+        newPeer.on('open', function(id) {
+          console.log("The peerID is: "+id);
+          resolve({peer:newPeer,peerId:id});
+        });  
+      })
+      newPeerPromise.then(result =>{
+        result.peer.on('connection', function(newConn) {
+          console.log("We just connected!");
+          setNewestConn(newConn);
+          setIsHost(true);
+        });
+        setMyPeerID(shortID);
+        setPeer(result.peer);
+      });
+    }
+  }
+  const setClient = () => {
+    if(myPeerID == null) {
+      var newPeer = new Peer();
+        newPeer.on('open', function(id) {
+          console.log("The peerID is: "+id);
+        setMyPeerID(id);
+        setPeer(newPeer);
+      })
+    }
+  }
+  const makeid = (length) => {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
   return (
     <>
       <h1>Hello World</h1>
+      <button onClick={setHost}>Be Host</button>
+      <button onClick={setClient}>Be Client</button><br />
+      {myPeerID}<br />
       <label>Connection ID: <input value={connectionID} name="connectionID" onChange={e => setConnectionID(e.target.value)} /></label><br />
       <button onClick={connectionButton}>Connect</button>
       <button onClick={sendAnotherMessageButton}>Send Another Message</button>
+      
+
+      <br />
+
     </>
   )
 }
